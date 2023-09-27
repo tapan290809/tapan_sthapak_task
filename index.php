9e334bda1726fb82cd97c7fc2670076c98b24660
tapan290809 <146164697+tapan290809@users.noreply.github.com>
2023-09-27 13:15:31 +0530

    password commit

--- index.php
+++ index.php
@@ -44,7 +44,7 @@ $resultcountrys = mysqli_query($con,$countrys);
                 </div>
                 <div class="mb-3">
                     <label for="">Password</label>
-                    <input type="text" name="password" class="form-control" />
+                    <input type="password" name="password" class="form-control" />
                 </div>
                 <div class="mb-3">
                     <label for="">Country</label>
